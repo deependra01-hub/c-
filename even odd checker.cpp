2fc9c94589591cd6e9fda5bb940fed0379d7c6a8
deependra01-hub <deependarsingh0474@gmail.com>
2026-01-02 21:40:54 +0530

    even-odd checker program in C++

---
+++ even odd checker.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"enter a no: ";
+    cin>>a;
+
+    if(a%2==0){
+        cout<<"no is even."<<endl;
+
+    }else{
+        cout<<"no is odd."<<endl;
+    }
+    return 0;
+}
