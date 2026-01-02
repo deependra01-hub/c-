d7648263c92d6a8a40e60c136b5a2c1417cdde38
deependra01-hub <deependarsingh0474@gmail.com>
2026-01-02 21:33:29 +0530

    conditional if-else statement for voting

---
+++ ifstatement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"enter your age: "<<endl;
+    cin>>a;
+
+    if (a>=18){
+        cout<<"eligible for voting"<< endl;
+
+    }
+    else{
+        cout<<"not eligible";
+    }
+}
