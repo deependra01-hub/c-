c7eb07de09ad46c7cc9337adc4999d51de54cb32
deependra01-hub <deependarsingh0474@gmail.com>
2026-01-02 17:00:35 +0530

    example of arithmetic and comparison operators
    Demonstrate arithmetic and comparison operators in C++.

---
+++ operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 10;
+    cout << "arithmetic operators in c++" << endl;
+    cout << "sum is :" << a + b << endl;        // addition operator
+    cout << "difference is :" << b - a << endl; // subtraction operator
+    cout << "product is :" << a * b << endl;    // multiplication operator
+    cout << "quotient is :" << b / a << endl;   // division operator
+    cout << endl
+         << endl;
+    cout << "comparison operators in c++" << endl;
+    cout << "is a equal to b :" << (a == b) << endl;     // equal to operator
+    cout << "is a not equal to b :" << (a != b) << endl; // not equal to operator
+    cout << "is a greater than b :" << (a > b) << endl;  // greater than operator
+}
