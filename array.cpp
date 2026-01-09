955064e49e479ea4a4709e519c28adbec781bcbb
deependra01-hub <deependarsingh0474@gmail.com>
2026-01-09 23:10:44 +0530

    Modify array values and use loop for output

--- array.cpp
+++ array.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int array[5] = {1, 2, 3, 4, 5};
-
-    cout<<array[0]<<endl;
-    cout<<array[1]<<endl;
-    cout<<array[2]<<endl;
-    cout<<array[3]<<endl;
-    cout<<array[4]<<endl;
-
-    return 0;
+    int array[5] = {10, 20, 30, 40, 50};
+    for (int i = 0; i < sizeof(array) / sizeof(int); i++)
+    {
+        cout << array[i] << endl;
+    }
 }
